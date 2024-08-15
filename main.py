62b0a7ad114a64cdd1af2f7b56fdd492fef354d6
LouisTheBest1231 <lemes3@ulaval.ca>
2024-08-15 14:16:10 -0400

    new html pages

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def home_page():
 @app.route("/lora_test")
 def lora_test_page():
     return app.send_static_file('car_test_set.html')
+
+@app.route("/lora_eval")
+def lora_eval_page():
+    return app.send_static_file('car_eval_set.html')
